300890aa1984d9dccfae2e69e0c7ea7fac159045
Helam Moreira <helam.dev@gmail.com>
2022-10-14 18:51:45 -0300

    implementing quotes table info

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,39 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
+import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import HomeIcon from '@mui/icons-material/Home';
+
 const Nav = () => {
     return (
-        <ul className="menu-block">
-            <li className="menu-item">
-                <Link to={'/'}>
-                    <div className="menu-item-icon"></div>
-                    <div className="menu-item-label">Dashboard</div>
+        <>
+        <Paper sx={{ 
+            width: 200, 
+            maxWidth: '100%',
+            height: '100vh' 
+        }}>
+            <MenuList>
+                <Link to={'/'} className="menu-item-action">
+                    <MenuItem>
+                        <ListItemIcon>
+                            <HomeIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText sx={{
+                            textAlign: 'left'
+                        }}>
+                            Dashboard
+                        </ListItemText>
+                    </MenuItem>
                 </Link>
-            </li>
-            <li className="menu-item">
-                <Link to={'/'}>
-                    <div className="menu-item-icon"></div>
-                    <div className="menu-item-label">Quotes</div>
-                </Link>
-            </li>
-        </ul>
+                <Divider/>
+            </MenuList>
+        </Paper>
+        </>
     );
 }
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -9,13 +9,62 @@ import TableRow from '@mui/material/TableRow';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function createData(id, name, departure, destination, travellers, transportation) {
     return { id, name, departure, destination, travellers, transportation };
 }
 
+const tableInfo = (quotes) =>{
+    if ( quotes.length < 1 ) {
+        return (<></>)
+    }
+    return (
+        <Table sx={{ minWidth: 800 }} aria-label="simple table">
+            <TableHead>
+            <TableRow>
+                <TableCell>#ID</TableCell>
+                <TableCell >Name</TableCell>
+                <TableCell >Departure</TableCell>
+                <TableCell >Destination</TableCell>
+                <TableCell >Travellers</TableCell>
+                <TableCell >Transportation</TableCell>
+                <TableCell >Action</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+                
+            {quotes.map((row) => (
+                <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                <TableCell component="th" scope="row">
+                    {row.id}
+                </TableCell>
+                <TableCell >{row.name}</TableCell>
+                <TableCell >{row.departure}</TableCell>
+                <TableCell >{row.destination}</TableCell>
+                <TableCell >{row.travellers}</TableCell>
+                <TableCell >{row.transportation}</TableCell>
+                <TableCell >
+                    <Link to={`/quote/${row.id}`} >
+                        <Button size="small">Detalhes</Button>
+                    </Link>
+                </TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+    );
+}
+
 const DashboardPage = () =>{
     const [quotes, setQuotes] = useState([]);
+    const [loader, setLoader] = useState(<CircularProgress/>);
 
     useEffect(()=>{
         setTimeout(()=>{
@@ -24,6 +73,7 @@ const DashboardPage = () =>{
             ];
 
             setQuotes(rows);
+            setLoader(<></>)
         }, 2000)
     });
 
@@ -32,44 +82,26 @@ const DashboardPage = () =>{
         <div >
             <h1>Dashboard</h1>
 
-            <Box sx={'auto'}>
+            <Box >
             <Grid 
                 container
-                direction="row"
+                direction="column"
                 justifyContent="center"
                 alignItems="center">
-                
-                <Grid item xs={4}>
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                            <TableRow>
-                                <TableCell>#ID</TableCell>
-                                <TableCell >Name</TableCell>
-                                <TableCell >Departure</TableCell>
-                                <TableCell >Destination</TableCell>
-                                <TableCell >Travellers</TableCell>
-                                <TableCell >Transportation</TableCell>
-                            </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {quotes.map((row) => (
-                                <TableRow
-                                key={row.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                <TableCell component="th" scope="row">
-                                    {row.id}
-                                </TableCell>
-                                <TableCell >{row.name}</TableCell>
-                                <TableCell >{row.departure}</TableCell>
-                                <TableCell >{row.destination}</TableCell>
-                                <TableCell >{row.travellers}</TableCell>
-                                <TableCell >{row.transportation}</TableCell>
-                                </TableRow>
-                            ))}
-                            </TableBody>
-                        </Table>
+
+
+                <Grid item xs={6}>
+                    <TableContainer 
+                        component={Paper}
+                        sx={{
+                            minWidth: '300px',
+                            minHeight: '300px'
+                        }}>
+                            
+                        <h3>Quotes</h3>
+                        <Divider />
+                        {loader}
+                        {tableInfo(quotes)}
                     </TableContainer>
                 </Grid>
             </Grid>
