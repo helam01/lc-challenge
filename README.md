3ade1bc0dac5c1b6f88830311a1624065e26d4f9
Helam Moreira <helam.dev@gmail.com>
2022-10-15 17:05:04 -0300

    implementing ORM on API

---
+++ README.md
@@ -0,0 +1,20 @@
+# Launch Code Challenge Aplication
+
+This repository has two aplications, one frontend created with ReactJS, and one Backend API created with NestJS.
+
+Both application use Nodejs v14.18
+
+## Starting Backend API
+Inside the directory 'wetbat-api' run:
+
+`npm install` to install it's dependencies.
+
+`npm run start:dev` to start the application on: [http://localhost:3030]
+
+
+## Starting Frontend App
+Inside the directory 'wetbat-frontend' run:
+
+`npm install` to install it's dependencies.
+
+`npm run start` to start the application on: [http://localhost:3000]
---
+++ wetbat-api/prisma/migrations/20221015195656_init/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE `Contact` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Contact_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Location` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Transportation` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Quote` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `contact_id` INTEGER NOT NULL,
+    `origin_id` INTEGER NOT NULL,
+    `destination_id` INTEGER NOT NULL,
+    `transportation_id` INTEGER NOT NULL,
+    `departure_date` VARCHAR(191) NOT NULL,
+    `return_date` VARCHAR(191) NOT NULL,
+    `travellers` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Quote` ADD CONSTRAINT `Quote_contact_id_fkey` FOREIGN KEY (`contact_id`) REFERENCES `Contact`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Quote` ADD CONSTRAINT `Quote_transportation_id_fkey` FOREIGN KEY (`transportation_id`) REFERENCES `Transportation`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Quote` ADD CONSTRAINT `Quote_origin_id_fkey` FOREIGN KEY (`origin_id`) REFERENCES `Location`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Quote` ADD CONSTRAINT `Quote_destination_id_fkey` FOREIGN KEY (`destination_id`) REFERENCES `Location`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
